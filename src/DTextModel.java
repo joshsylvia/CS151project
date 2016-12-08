a484ae9ba386f649b900804e605d01b01508b65f
Josh <josh.sylvia@sjsu.edu>
2016-12-08 15:09:46 -0800

    Created draw() methods and built model for Dtext().

--- src/DTextModel.java
+++ src/DTextModel.java
@@ -1,17 +1,21 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
 
 public class DTextModel extends DShapeModel {
 
     public DTextModel() {
 
     }
-   // public static void showKnob(Graphics g) {
-    /*    if(DShape.isSelected) {
+   
+    public static void showKnob(Graphics g) {
+    //    if(DShape.isSelected) {
             Rectangle[] knob = getKnobs();
             for (int i = 0; i < knobs.length; i++) {
                 g.setColor(Color.BLACK);
                 g.fillRect(knob[i].x, knob[i].y, knob[i].width, knob[i].height);
 
         }
-        */
-  //  }
+        
+    }
 }
