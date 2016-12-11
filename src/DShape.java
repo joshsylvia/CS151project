e6065a40482f74d0b74c69780c522c69dda7402d
Michael <Michael@Pearl>
2016-12-10 19:43:07 -0800

    Linked DShape to register ModelListener on DShapeModel

--- src/DShape.java
+++ src/DShape.java
@@ -1,11 +1,12 @@
 import java.io.Serializable;
 
-public class DShape implements Serializable {
+public class DShape implements Serializable, ModelListener {
     public DShapeModel model;
     public static boolean isSelected = false;
 
     public DShape(DShapeModel smodel) {
         model = smodel;
+        model.register(this);
     }
 
     public void setModel(DShapeModel smodel) {
@@ -15,4 +16,13 @@ public class DShape implements Serializable {
     public DShapeModel getModel() {
         return model;
     }
+
+	@Override
+	public void modelChanged(DShapeModel model) {
+		// need a method here to prompt the shape 
+		// to repaint itself on the canvas and it
+		// will do so whenever a change to its model
+		// is detected
+		
+	}
 }
